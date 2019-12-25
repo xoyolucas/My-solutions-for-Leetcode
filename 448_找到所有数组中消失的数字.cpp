9731a5297dd601873f2d8e4c5a859b8f91b9941b
me <xoyolucas@outlook.com>
2019-12-26 01:26:24 +0800

    448_找到所有数组中消失的数字

---
+++ 448_找到所有数组中消失的数字.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    vector<int> findDisappearedNumbers(vector<int> &nums)
+    {
+        vector<int> res;
+        for (auto &num : nums)
+        {
+            nums[abs(num) - 1] = -1 * abs(nums[abs(num) - 1]);
+        }
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (nums[i] > 0)
+            {
+                res.push_back(i + 1);
+            }
+        }
+        return res;
+    }
+};
