dc7c1cb08036e8191e03bc2a4cfd1caeeb3fba8f
me <xoyolucas@outlook.com>
2020-02-05 01:21:16 +0800

    232.用栈实现队列

---
+++ 232.用栈实现队列.cpp
@@ -0,0 +1,114 @@
+/*
+ * @lc app=leetcode.cn id=232 lang=cpp
+ *
+ * [232] 用栈实现队列
+ *
+ * https://leetcode-cn.com/problems/implement-queue-using-stacks/description/
+ *
+ * algorithms
+ * Easy (62.05%)
+ * Likes:    127
+ * Dislikes: 0
+ * Total Accepted:    30.3K
+ * Total Submissions: 48.6K
+ * Testcase Example:  '["MyQueue","push","push","peek","pop","empty"]\n[[],[1],[2],[],[],[]]'
+ *
+ * 使用栈实现队列的下列操作：
+ * 
+ * 
+ * push(x) -- 将一个元素放入队列的尾部。
+ * pop() -- 从队列首部移除元素。
+ * peek() -- 返回队列首部的元素。
+ * empty() -- 返回队列是否为空。
+ * 
+ * 
+ * 示例:
+ * 
+ * MyQueue queue = new MyQueue();
+ * 
+ * queue.push(1);
+ * queue.push(2);  
+ * queue.peek();  // 返回 1
+ * queue.pop();   // 返回 1
+ * queue.empty(); // 返回 false
+ * 
+ * 说明:
+ * 
+ * 
+ * 你只能使用标准的栈操作 -- 也就是只有 push to top, peek/pop from top, size, 和 is empty
+ * 操作是合法的。
+ * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+ * 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）。
+ * 
+ * 
+ */
+
+// @lc code=start
+#include <stack>
+using namespace std;
+
+class MyQueue
+{
+public:
+    /** Initialize your data structure here. */
+    MyQueue()
+    {
+    }
+
+    /** Push element x to the back of queue. */
+    void push(int x)
+    {
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    int pop()
+    {
+        if (stack2.empty())
+        {
+            while (!stack1.empty())
+            {
+                stack2.push(stack1.top());
+                stack1.pop();
+            }
+        }
+
+        int num = stack2.top();
+        stack2.pop();
+        return num;
+    }
+
+    /** Get the front element. */
+    int peek()
+    {
+        if(stack2.empty())  
+            while(!stack1.empty())
+            {
+                stack2.push(stack1.top());
+                stack1.pop();
+            }
+        int num = stack2.top();
+        return num;
+    }
+
+    /** Returns whether the queue is empty. */
+    bool empty()
+    {
+        return stack1.empty() && stack2.empty();
+    }
+
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
+
+//需要用到两个栈，用它们其中一个来反转元素的入队顺序，用另一个来存储元素的最终顺序。
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+// @lc code=end
