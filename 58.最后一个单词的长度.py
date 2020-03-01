fdd225ac3b55b2ba31bacd36006ac778d8a9993a
me <xoyolucas@outlook.com>
2020-03-02 00:25:57 +0800

    58.最后一个单词的长度

---
+++ 58.最后一个单词的长度.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode.cn id=58 lang=python
+#
+# [58] 最后一个单词的长度
+#
+# https://leetcode-cn.com/problems/length-of-last-word/description/
+#
+# algorithms
+# Easy (32.07%)
+# Likes:    177
+# Dislikes: 0
+# Total Accepted:    71K
+# Total Submissions: 218.1K
+# Testcase Example:  '"Hello World"'
+#
+# 给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。
+# 
+# 如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+# 
+# 如果不存在最后一个单词，请返回 0 。
+# 
+# 说明：一个单词是指仅由字母组成、不包含任何空格的 最大子字符串。
+# 
+# 
+# 
+# 示例:
+# 
+# 输入: "Hello World"
+# 输出: 5
+# 
+# 
+#
+
+# @lc code=start
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip(' ')
+        L = s.split(' ')[-1]
+        return len(L)
+
+# @lc code=end
+
