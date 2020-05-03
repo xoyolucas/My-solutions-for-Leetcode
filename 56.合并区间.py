74df0a3638d1629080bef8fa96b347784a2b3524
me <xoyolucas@outlook.com>
2020-05-03 18:15:58 +0800

    56.合并区间.py

---
+++ 56.合并区间.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode.cn id=56 lang=python3
+#
+# [56] 合并区间
+#
+# https://leetcode-cn.com/problems/merge-intervals/description/
+#
+# algorithms
+# Medium (40.47%)
+# Likes:    330
+# Dislikes: 0
+# Total Accepted:    65.3K
+# Total Submissions: 160K
+# Testcase Example:  '[[1,3],[2,6],[8,10],[15,18]]'
+#
+# 给出一个区间的集合，请合并所有重叠的区间。
+#
+# 示例 1:
+#
+# 输入: [[1,3],[2,6],[8,10],[15,18]]
+# 输出: [[1,6],[8,10],[15,18]]
+# 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+#
+#
+# 示例 2:
+#
+# 输入: [[1,4],[4,5]]
+# 输出: [[1,5]]
+# 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+#
+#
+
+# @lc code=start
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        res = []
+
+        for element in intervals:
+            if len(res) == 0 or res[-1][1] < element[0]:
+                res.append(element)
+            else:
+                res[-1][1] = max(res[-1][1], element[1])
+
+        return res
+
+
+# @lc code=end
