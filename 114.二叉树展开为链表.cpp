a2c9642853e5bffc5b11bda4b558b96fd937b8c1
me <xoyolucas@outlook.com>
2020-08-16 18:19:51 +0800

    114.二叉树展开为链表.cpp

---
+++ 114.二叉树展开为链表.cpp
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=cpp
+ *
+ * [114] 二叉树展开为链表
+ *
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/description/
+ *
+ * algorithms
+ * Medium (70.90%)
+ * Likes:    497
+ * Dislikes: 0
+ * Total Accepted:    73.3K
+ * Total Submissions: 103.4K
+ * Testcase Example:  '[1,2,5,3,4,null,6]'
+ *
+ * 给定一个二叉树，原地将它展开为一个单链表。
+ * 
+ * 
+ * 
+ * 例如，给定二叉树
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   5
+ * ⁠/ \   \
+ * 3   4   6
+ * 
+ * 将其展开为：
+ * 
+ * 1
+ * ⁠\
+ * ⁠ 2
+ * ⁠  \
+ * ⁠   3
+ * ⁠    \
+ * ⁠     4
+ * ⁠      \
+ * ⁠       5
+ * ⁠        \
+ * ⁠         6
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    void flatten(TreeNode *root)
+    {
+        TreeNode *cur = root;
+        while (cur != nullptr)
+        {
+            TreeNode *left = cur->left;
+            TreeNode *right = left;
+            if (right != nullptr)
+            {
+                while (right->right != nullptr)
+                {
+                    right = right->right;
+                }
+                right->right = cur->right;
+                cur->right = left;
+                cur->left = nullptr;
+            }
+            cur = cur->right;
+        }
+    }
+};
+// @lc code=end
