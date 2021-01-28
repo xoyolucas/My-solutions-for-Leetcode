9187300027f8e0d5b412da73d34986287a83d60d
me <xoyolucas@outlook.com>
2021-01-28 22:16:22 +0800

    74.搜索二维矩阵.py

---
+++ 74.搜索二维矩阵.py
@@ -0,0 +1,70 @@
+#
+# @lc app=leetcode.cn id=74 lang=python3
+#
+# [74] 搜索二维矩阵
+#
+# https://leetcode-cn.com/problems/search-a-2d-matrix/description/
+#
+# algorithms
+# Medium (39.67%)
+# Likes:    297
+# Dislikes: 0
+# Total Accepted:    77.3K
+# Total Submissions: 194.9K
+# Testcase Example:  '[[1,3,5,7],[10,11,16,20],[23,30,34,60]]\n3'
+#
+# 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+#
+#
+# 每行中的整数从左到右按升序排列。
+# 每行的第一个整数大于前一行的最后一个整数。
+#
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+# 输出：true
+#
+#
+# 示例 2：
+#
+#
+# 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+# 输出：false
+#
+#
+#
+#
+# 提示：
+#
+#
+# m == matrix.length
+# n == matrix[i].length
+# 1
+# -10^4
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+
+        low, high = 0, m*n-1
+        while low <= high:
+            # 二分查找
+            mid = low+(high-low)//2
+            if matrix[mid//n][mid % n] == target:
+                return True
+            elif matrix[mid//n][mid % n] > target:
+                high = mid-1
+            else:
+                low = mid+1
+                
+        return False
+# @lc code=end
